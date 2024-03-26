989e86cd44e5d828e2b5413ef76ea13565105b9c
gngugnniokl <75842539+gngugnniokl@users.noreply.github.com>
2024-03-26 15:52:35 +0100

    New commit

---
+++ week-2/practice_3.py
@@ -0,0 +1,9 @@
+list = [ 'Anaconda', 786 , 2.23, 'Jupyter', 70.2 ]
+shortlist = [321, 'Python']
+
+print(list)  #Prints  complete list#
+print(list[0]) #Prints  first element of the list#
+print(list[1:3]) #Prints  elements starting from 2nd#
+print(list[2:]) #Prints  elements starting from the 3rd#
+print(shortlist * 2) #prints list 2 times#
+print(list + shortlist)#prints concatenated lists
---
+++ week-2/practice_7.py
@@ -0,0 +1,9 @@
+# Convert to int
+
+grade = float(97) #grade will be 70
+gpa = float(5) #grade will be 4.9
+cgpa = float("4.7") #cgpa will be 4
+
+print(f"Grade = {grade}")
+print(f"GPA = {gpa}")
+print(f"CGPA = {cgpa}")
